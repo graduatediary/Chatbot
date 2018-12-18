74fb12eb56c1317f243ed2c8793eef05ce80b85b
unknown <remember2023@gmail.com>
2018-12-18 18:08:08 +0900

    201812181808

--- day2/telegram.py
+++ day2/telegram.py
@@ -1,6 +1,21 @@
+import requests
+from time import sleep
+import bs4
+
+
+url = "https://www.naver.com"
+response = requests.get(url)
+result = bs4.BeautifulSoup(response.text,'html.parser')
+value = result.select_one('.PM_CL_realtimeKeyword_rolling .ah_item .ah_k'  )
 
 key = "799893065:AAF671iykHhvFZ9X-LUr6wvryRRDnl55JXY"
 user_id = "724888484"
+message= value.text
+#"Hope is a good thing, maybe the best of things, and no good thing ever dies."
+#"224026642"
 
-url = f"https://api.telegram.org/bot{key}/getMe"
+url=f"https://api.telegram.org/bot{key}/sendMessage?chat_id={user_id}&text={message}"
 
+while True:
+    sleep(1)
+    requests.get(url)
---
+++ day2/test.py
@@ -0,0 +1,12 @@
+url = 'https://www.bithumb.com/'
+
+import requests
+from bs4 import BeautifulSoup
+html = requests.get(url).text
+soup = BeautifulSoup(html, 'html.parser')
+for tag in soup.select('.coin_list tr'):
+  name = tag.select_one('td:nth-of-type(1) a strong').text.replace(' NEW',' ').strip()
+  price = tag.select_one('td:nth-of-type(2) strong').text
+  print(name + " / " + price)
+  f = open("./a.txt", 'a+')
+  f.write(name + " / " + price + '\n')
